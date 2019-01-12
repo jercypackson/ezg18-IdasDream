c47689130430b1bc39c6c188e79bcf3904424568
Laura Luidolt <laura.luidolt@gmail.com>
2019-01-12 23:14:45 +0100

    animation and textures and fixes

--- IdasDream/Main.cpp
+++ IdasDream/Main.cpp
@@ -8,7 +8,41 @@
 
 int main(int argc, char** argv)
 {
-	// Read settings
+
+	/*
+	 *todo: remove
+	 */
+	 //	std::vector<int> secs = {
+	 //		1750,
+	 //1810 ,
+	 //1820 ,
+	 //1825 ,
+	 //1845 ,
+	 //1880 ,
+	 //1880 ,
+	 //1915 ,
+	 //1925 ,
+	 //1935 ,
+	 //2005 ,
+	 //2015 ,
+	 //2025 ,
+	 //2155 ,
+	 //2165 ,
+	 //2175
+	 //	};
+	 //
+	 //	int zero = 5310;
+	 //
+	 //	std::cout << zero << "," << std::endl;
+	 //
+	 //	for (int i = 0; i < secs.size() - 1; ++i)
+	 //	{
+	 //		zero += secs[i + 1] - secs[i];
+	 //		std::cout << zero << "," << std::endl;
+	 //	}
+
+
+		  // Read settings
 	INIReader reader(Extensions::assets + "settings.ini");
 
 	std::unique_ptr<IdasDream> app = std::make_unique<IdasDream>(
@@ -16,7 +50,7 @@ int main(int argc, char** argv)
 		reader.GetInteger("WindowParams", "height", 900),
 		reader.GetBoolean("WindowParams", "fullscreen", false),
 		reader.GetInteger("WindowParams", "samples", 1)
-	);
+		);
 
 	if (!app->start()) {
 		std::getchar();
